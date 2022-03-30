a5e0528ae5891221633272c745b9dd2beccefa79
Andi Rustianto <anditodev46@gmail.com>
2022-03-30 18:48:46 +0700

    validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
 const User = require("../model/User");
 
+// validation
+const Joi = require("@hapi/joi");
+
+const schema = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().min(3).required().email(),
+  password: Joi.string().min(3).required(),
+});
+
 router.post("/register", async (req, res) => {
+  // validate the data before we a user
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
